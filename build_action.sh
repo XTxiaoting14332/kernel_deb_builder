3c81ddaad118b8731ef1ebf952aa8b3f32bd5a46
XTxiaoting14332 <HT30332@126.com>
2024-06-15 23:21:33 +0800

    fix

--- build_action.sh
+++ build_action.sh
@@ -6,7 +6,7 @@ sed -i "/deb-src/s/# //g" /etc/apt/sources.list
 
 # install dep
 apt update
-apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev aria2 build-essential devscripts dwarves
+apt install -y wget zstd xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev aria2 build-essential devscripts dwarves
 apt build-dep -y linux
 
 # change dir to workplace
